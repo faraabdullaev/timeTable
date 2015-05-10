b6ca064f4a1b7c989ea829973bf7c8a2eaa4e5ad
farrukh <faraabdullaev@gmail.com>
2015-05-10 18:36:10 +0500

    db 2

--- protected/config/db_config.php
+++ protected/config/db_config.php
@@ -3,11 +3,12 @@
 return array(
 	'components'=>array(
 		'db'=>array(
-			'connectionString' => 'pgsql:'.
-				'host=ec2-54-225-154-5.compute-1.amazonaws.com;'.
-				'port=5432;'.
-				'dbname=d482a7prsubscl',
-			'emulatePrepare' => true,
+			'connectionString' => 'pgsql:'
+				. 'host=ec2-54-225-154-5.compute-1.amazonaws.com;'
+				. 'port=5432;'
+				. 'dbname=d482a7prsubscl',
+
+//			'emulatePrepare' => true,
 			'username' => 'xdzkljkowyigcy',
 			'password' => 'MUdqtcF263XVFSWeKc8T9AR254',
 			'charset' => 'utf8',
